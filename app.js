1409736f9b99ec0eb519e94eaf859c317e13db63
dyrachokk bc2024-3 <mdrigel9@gmail.com>
2024-11-03 15:30:09 +0200

    W

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ program.parse(process.argv);
 
 const options = program.opts();
 
-// Перевірка наявності обов'язкового файлу
+
 if (!fs.existsSync(options.input)) {
   console.error('Cannot find input file');
   process.exit(1);
 }
 
-// Зчитування файлу
+
 const data = fs.readFileSync(options.input, 'utf8');
 
-// Виведення або запис даних
+
 if (options.output) {
   fs.writeFileSync(options.output, data, 'utf8');
 }
@@ -27,7 +27,7 @@ if (options.display) {
   console.log(data);
 }
 
-// Якщо не задано -o або -d, програма нічого не виводить
+
 if (!options.input) {
     console.error('Please, specify input file');
     process.exit(1);
